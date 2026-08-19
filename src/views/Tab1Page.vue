f5f1918c3e1e6ba4c89c72ef5f495620ee7f2e9b
vinaonepai <viniciusouza443@gmail.com>
2026-08-19 14:55:50 -0300

    fix: erro no código

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -20,4 +20,4 @@
 <script setup lang="ts">
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
 import ExploreContainer from '@/components/ExploreContainer.vue';
-;</script>
+</script>
